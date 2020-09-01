e183bb4bcd08ee6066f27d9d68145aaa3ef67abf
Nikolay19862106 <black21.06.1986@gmail.com>
2020-09-01 21:45:45 +0600

    Add project

---
+++ DZ/DZ/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число, которое хотите возвести в степень");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите степень");
+            int stepen = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"{x}^{stepen} = " + Math.Pow(x, stepen));
+            Console.ReadKey();
+        }
+    }
+}
+
